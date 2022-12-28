23302d4a842d8af098433c0c5bd75ac83876081b
ivinogradov <ilya@protonicapps.com>
2022-12-28 17:37:16 -0600

    Converted App function to class; created FlatList

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes will automatically reload!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ["Harry", "James", "Bobby", "Carl", "Jos"]
+    }
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={ this.state.data }
+          renderItem={({item, separators}) => (
+            <View>
+              <Text>{item}</Text>
+            </View>
+          )}
+          keyExtractor={ (item, index) => index.toString()}
+          />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff000',
+    marginTop: 50,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
