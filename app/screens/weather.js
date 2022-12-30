883c6b0dbc95d094a5e5d6a94396520bc0a5dc22
ivinogradov <ilya.vngrdv@gmail.com>
2022-12-30 14:18:40 -0600

    Load temperature for 5 cities + Documentation

--- app/screens/weather.js
+++ app/screens/weather.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, unstable_batchedUpdates } from 'react-native';
 import { WEATHER_API_KEY } from '../../weather_api_key';
 
 export default function Weather () {
@@ -16,8 +16,13 @@ export default function Weather () {
     );
 }
 
+/**
+ * Fetching temperatures for a 6 random cities
+ * @precondition API restriction 6 calls/minute - can be refreshed up to 10 times in a minute
+ * @param {function} setPickedCities state setter function accepting an array of cities
+ */
 const fetchTemps = (setPickedCities) => {
-    var list = getRandomCities(cities, 1);
+    var list = getRandomCities(cities, 6);
     var fetchedTemps = [];
     for (const city in list) {
         if (Object.hasOwnProperty.call(list, city)) {
@@ -30,6 +35,8 @@ const fetchTemps = (setPickedCities) => {
  * Fetches data from open weather map API
  * @param {string} city city name
  * @param {string} country country name or abbreviation
+ * @param {object[]} citiesTempsList array of city orjects containing weather data for each city
+ * @param {function} setPickedCities state setter function accepting an array of cities
  */
 const fetchCityTemp = ( city, country, citiesTempsList, setPickedCities ) => {
     fetch('https://api.openweathermap.org/data/2.5/weather?q='
