0721bb98319dfdf3fed798a76bc8b48ce62fbff8
ivinogradov <ilya.vngrdv@gmail.com>
2022-12-29 20:14:41 -0600

    Refactor out people into its own file

--- App.js
+++ App.js
@@ -1,87 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Weather from './app/screens/weather';
-
-export class ListTab extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [{
-        name: "Old",
-        image: "http://i.pravatar.cc/400?img=10"
-      }, {
-        name: "Data",
-        image: "http://i.pravatar.cc/400?img=20"
-      }],
-      refreshing: false
-    }
-  }
-
-  refreshData = () => {
-    this.setState({
-      refreshing: true
-    })
-
-    this.setState({
-      refreshing: false,
-      data: [{
-        name: "Marina",
-        image: "http://i.pravatar.cc/400?img=1"
-      }, {
-        name: "James",
-        image: "http://i.pravatar.cc/400?img=2"
-      }, {
-        name:  "Bobby",
-        image: "http://i.pravatar.cc/400?img=3"
-      }, {
-        name:  "Carl",
-        image: "http://i.pravatar.cc/400?img=4"
-      }, {
-        name:  "Mila",
-        image: "http://i.pravatar.cc/400?img=5"
-      }, {
-        name:  "Ilya",
-        image: "http://i.pravatar.cc/400?img=6"
-      }, {
-        name:  "Josh",
-        image: "http://i.pravatar.cc/400?img=7"
-      }, {
-        name:  "Garry",
-        image: "http://i.pravatar.cc/400?img=8"
-      }, {
-        name:  "Jos",
-        image: "http://i.pravatar.cc/400?img=9"
-      }]
-    })
-  }
-
-  render() {
-    return(
-      <View style={styles.container}>
-        <FlatList
-          style={{ width: '100%' }}
-          data={ this.state.data }
-          refreshing={ this.state.refreshing }
-          onRefresh={ this.refreshData }
-          renderItem={({item, index }) => (
-            <View style={[ styles.rowStyle, index % 2 ? styles.itemOdd : styles.itemEven ]}>
-              <Image
-                style={ styles.image }
-                source={{ uri: item.image }}
-              />
-              <Text style={{ marginLeft: 25, lineHeight: 50, fontWeight: 'bold', color: 'blue', opacity: 0.5 }}>{item.name}</Text>
-            </View>
-          )}
-          keyExtractor={ (item, index) => index.toString()}
-          />
-        <StatusBar style="auto" />
-      </View>
-    )
-  }
-}
+import { People } from './app/screens/people';
 
 const Tab = createBottomTabNavigator()
 
@@ -90,37 +12,11 @@ export default class App extends React.Component {
     return (
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name='People' component={ListTab}/>
+          <Tab.Screen name='People' component={People}/>
           <Tab.Screen name='Random Weather' component={Weather}/>
         </Tab.Navigator>
+        <StatusBar style="auto" />
       </NavigationContainer>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  image: {
-    borderRadius: 56/2,
-    height: 56,
-    width: 56
-  },
-  rowStyle: {
-    flexDirection: 'row',
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    borderBottomWidth: 1,
-    borderColor: 'lightgrey'
-  },
-  itemOdd: {
-    backgroundColor: '#f1f1f1'
-  },
-  itemEven: {
-    backgroundColor: '#ffffff'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/screens/people.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+
+export class People extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: [{
+          name: "Old",
+          image: "http://i.pravatar.cc/400?img=10"
+        }, {
+          name: "Data",
+          image: "http://i.pravatar.cc/400?img=20"
+        }],
+        refreshing: false
+      }
+    }
+  
+    refreshData = () => {
+      this.setState({
+        refreshing: true
+      })
+  
+      this.setState({
+        refreshing: false,
+        data: [{
+          name: "Marina",
+          image: "http://i.pravatar.cc/400?img=1"
+        }, {
+          name: "James",
+          image: "http://i.pravatar.cc/400?img=2"
+        }, {
+          name:  "Bobby",
+          image: "http://i.pravatar.cc/400?img=3"
+        }, {
+          name:  "Carl",
+          image: "http://i.pravatar.cc/400?img=4"
+        }, {
+          name:  "Mila",
+          image: "http://i.pravatar.cc/400?img=5"
+        }, {
+          name:  "Ilya",
+          image: "http://i.pravatar.cc/400?img=6"
+        }, {
+          name:  "Josh",
+          image: "http://i.pravatar.cc/400?img=7"
+        }, {
+          name:  "Garry",
+          image: "http://i.pravatar.cc/400?img=8"
+        }, {
+          name:  "Jos",
+          image: "http://i.pravatar.cc/400?img=9"
+        }]
+      })
+    }
+  
+    render() {
+      return(
+        <View style={styles.container}>
+          <FlatList
+            style={{ width: '100%' }}
+            data={ this.state.data }
+            refreshing={ this.state.refreshing }
+            onRefresh={ this.refreshData }
+            renderItem={({item, index }) => (
+              <View style={[ styles.rowStyle, index % 2 ? styles.itemOdd : styles.itemEven ]}>
+                <Image
+                  style={ styles.image }
+                  source={{ uri: item.image }}
+                />
+                <Text style={{ marginLeft: 25, lineHeight: 50, fontWeight: 'bold', color: 'blue', opacity: 0.5 }}>{item.name}</Text>
+              </View>
+            )}
+            keyExtractor={ (item, index) => index.toString()}
+            />
+        </View>
+      )
+    }
+}
+  
+const styles = StyleSheet.create({
+image: {
+    borderRadius: 56/2,
+    height: 56,
+    width: 56
+},
+rowStyle: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey'
+},
+itemOdd: {
+    backgroundColor: '#f1f1f1'
+},
+itemEven: {
+    backgroundColor: '#ffffff'
+},
+container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+});
