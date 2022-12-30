d6e5f5275c7cacd54387576fbb41ae99490593a4
ivinogradov <ilya.vngrdv@gmail.com>
2022-12-29 20:53:16 -0600

    Implemented weather API fetch

--- app/screens/weather.js
+++ app/screens/weather.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { WEATHER_API_KEY } from '../../weather_api_key';
 
 export default function Weather () {
+    fetchCityTemp('London', 'UK');
     return (
         <View>
             <Text>Weather Tab</Text>
         </View>
     );
+}
+
+var fetchCityTemp = ( city, country ) => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='
+    + city + ',' + country +
+    '&appid=' + WEATHER_API_KEY + '&units=metric')
+    .then((response) => response.json())
+    .then((responseJson) => {
+        var city = {
+            name: responseJson.name,
+            country: country,
+            temp: Math.ceil(responseJson.main.temp),
+            type: responseJson.weather[0].main
+        };
+        console.log(city);
+    });
 }
